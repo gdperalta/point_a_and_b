9d0836489a8abb108de1e2e64f59c28ac0f8db47
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-06-10 01:04:41 +0800

    Remade function of code such that it will not list down rates that are nil

--- app/javascript/packs/shipment_quotes/new.js
+++ app/javascript/packs/shipment_quotes/new.js
@@ -1,8 +1,10 @@
 import { assignCurrency } from '../utils/assignCurrency';
+import { filterData, assignData } from '../utils/handleData';
 
 class ComputeRates {
 	constructor() {
-		this.ratesColumn = document.querySelectorAll('.rates_column');
+		this.courierTable = document.getElementById('courier_table');
+		this.couriers = JSON.parse(this.courierTable.dataset.couriers);
 		[this.pickupProvince, this.deliveryProvince] =
 			document.querySelectorAll('.province_input');
 		this.computeBtn = document.getElementById('compute_button');
@@ -10,28 +12,53 @@ class ComputeRates {
 	}
 
 	addEvent = () => {
-		this.computeBtn.addEventListener('click', (e) => this.iterateRates(e));
+		this.computeBtn.addEventListener('click', (e) => this.iterateCouriers(e));
 	};
 
-	iterateRates = (e) => {
+	iterateCouriers = (e) => {
 		e.preventDefault();
+		this.courierTable.innerHTML = '';
 
-		this.ratesColumn.forEach((rate) => {
-			this.assignRates(rate);
+		// TODO create proper error message
+		if (this.pickupProvince.value.toUpperCase() !== 'METRO MANILA') {
+			this.errorMessage();
+			return;
+		}
+
+		this.couriers.forEach((courier) => {
+			courier.rate = assignCurrency('Php', this.assignRates(courier));
+			if (courier.rate.includes('null')) return;
+
+			this.addFields(courier);
 		});
 	};
 
-	assignRates = (rate) => {
-		rate.innerText = this.isWithinMetroManila()
-			? assignCurrency('Php', rate.dataset.withinMetroManila)
-			: assignCurrency('Php', rate.dataset.outsideMetroManila);
+	addFields = (resource) => {
+		let row = document.createElement('tr');
+		let filteredData = filterData(resource);
+
+		assignData(filteredData, row);
+		this.courierTable.appendChild(row);
+	};
+
+	errorMessage = () => {
+		let row = document.createElement('tr');
+		let dataColumn = document.createElement('td');
+		dataColumn.colSpan = '3';
+		dataColumn.innerText =
+			'We currently have no shipments available for pickup addresses outside Metro Manila';
+		row.appendChild(dataColumn);
+		this.courierTable.appendChild(row);
+	};
+
+	assignRates = (courier) => {
+		return this.isWithinMetroManila()
+			? courier.within_metro_manila
+			: courier.outside_metro_manila;
 	};
 
 	isWithinMetroManila = () => {
-		return (
-			this.pickupProvince.value === 'Metro Manila' &&
-			this.pickupProvince.value === this.deliveryProvince.value
-		);
+		return this.pickupProvince.value === this.deliveryProvince.value;
 	};
 }
 
