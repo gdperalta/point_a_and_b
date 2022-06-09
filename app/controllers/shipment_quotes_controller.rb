1567e122e39a93652fe4f2a7545a2ca1a3939405
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-06-09 22:28:47 +0800

    Add build methods for pickup and delivery address for nested fields in new method Add strong parameters for nested attributes Remove unusded comments and whitespaces Formatted with rubocop

--- app/controllers/shipment_quotes_controller.rb
+++ app/controllers/shipment_quotes_controller.rb
@@ -1,31 +1,28 @@
 class ShipmentQuotesController < ApplicationController
-  before_action :set_shipment_quote, only: %i[ show edit update destroy ]
+  before_action :set_shipment_quote, only: %i[show edit update destroy]
 
-  # GET /shipment_quotes or /shipment_quotes.json
   def index
     @shipment_quotes = ShipmentQuote.all
   end
 
-  # GET /shipment_quotes/1 or /shipment_quotes/1.json
-  def show
-  end
+  def show; end
 
-  # GET /shipment_quotes/new
   def new
     @shipment_quote = ShipmentQuote.new
+    @shipment_quote.build_pickup_address
+    @shipment_quote.build_delivery_address
   end
 
-  # GET /shipment_quotes/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /shipment_quotes or /shipment_quotes.json
   def create
     @shipment_quote = ShipmentQuote.new(shipment_quote_params)
 
     respond_to do |format|
       if @shipment_quote.save
-        format.html { redirect_to shipment_quote_url(@shipment_quote), notice: "Shipment quote was successfully created." }
+        format.html do
+          redirect_to shipment_quote_url(@shipment_quote), notice: 'Shipment quote was successfully created.'
+        end
         format.json { render :show, status: :created, location: @shipment_quote }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -34,11 +31,12 @@ class ShipmentQuotesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /shipment_quotes/1 or /shipment_quotes/1.json
   def update
     respond_to do |format|
       if @shipment_quote.update(shipment_quote_params)
-        format.html { redirect_to shipment_quote_url(@shipment_quote), notice: "Shipment quote was successfully updated." }
+        format.html do
+          redirect_to shipment_quote_url(@shipment_quote), notice: 'Shipment quote was successfully updated.'
+        end
         format.json { render :show, status: :ok, location: @shipment_quote }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -47,24 +45,27 @@ class ShipmentQuotesController < ApplicationController
     end
   end
 
-  # DELETE /shipment_quotes/1 or /shipment_quotes/1.json
   def destroy
     @shipment_quote.destroy
 
     respond_to do |format|
-      format.html { redirect_to shipment_quotes_url, notice: "Shipment quote was successfully destroyed." }
+      format.html { redirect_to shipment_quotes_url, notice: 'Shipment quote was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_shipment_quote
-      @shipment_quote = ShipmentQuote.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def shipment_quote_params
-      params.fetch(:shipment_quote, {})
-    end
+  def set_shipment_quote
+    @shipment_quote = ShipmentQuote.find(params[:id])
+  end
+
+  def shipment_quote_params
+    params.require(:shipment_quote).permit(:pickup_address_id, :delivery_address_id,
+                                           pickup_address_attributes: %i[id address1 address2 city province
+                                                                         country zip_code address_type],
+                                           delivery_address_attributes: %i[id address1 address2 city province
+                                                                           country zip_code address_type])
+
+  end
 end
