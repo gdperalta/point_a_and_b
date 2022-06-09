6a126571840953f9515fc7c462f35717686f076b
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-06-10 01:04:55 +0800

    Add documentation

--- app/javascript/packs/utils/assignCurrency.js
+++ app/javascript/packs/utils/assignCurrency.js
@@ -1,3 +1,9 @@
+/**
+ *
+ * @param {String} currency - Currency to be used (Php, Usd)
+ * @param {Number} rate - The price or rate to be formatted
+ * @returns {String} - Returns formatted amount with currency
+ */
 export const assignCurrency = (currency, rate) => {
 	return `${currency} ${rate}`;
 };
