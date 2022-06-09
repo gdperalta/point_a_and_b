3a056155315769ce4f063cf2c2234602c1c17477
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-06-09 22:34:05 +0800

    Add couriers method to handle courier rates for each shipment_quote

--- app/models/shipment_quote.rb
+++ app/models/shipment_quote.rb
@@ -1,4 +1,21 @@
 class ShipmentQuote < ApplicationRecord
-  has_one :pickup_address
-  has_one :delivery_address
+  belongs_to :pickup_address, class_name: 'Address'
+  belongs_to :delivery_address, class_name: 'Address'
+  accepts_nested_attributes_for :pickup_address
+  accepts_nested_attributes_for :delivery_address
+
+  def couriers
+    CourierRate.all.map do |courier|
+      courier_details = courier.attributes.symbolize_keys.slice(:name, :delivery_option)
+      courier_details[:rate] = courier_rate(courier)
+
+      courier_details
+    end
+  end
+
+  def courier_rate(courier)
+    return unless pickup_address.province.upcase == 'METRO MANILA'
+
+    pickup_address.province == delivery_address.province ? courier.within_metro_manila : courier.outside_metro_manila
+  end
 end
