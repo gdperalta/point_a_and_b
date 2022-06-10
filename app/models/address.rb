02bcd7f096ba1e0434917ff2dbf61c1c21d6c992
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-06-10 20:50:46 +0800

    Add validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,9 @@ class Address < ApplicationRecord
   enum address_type: %i[pickup delivery]
   has_many :shipment_quotes
   belongs_to :city
+  validates :address1, presence: true
+  validates :city, presence: true
+  validates :zip_code, presence: true
 
   def city_name
     city.name
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,4 @@
 class Country < ApplicationRecord
   has_many :provinces
+  validates :name, presence: true
 end
