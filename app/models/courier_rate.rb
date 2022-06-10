53fb5ce24315d34c225c7e3f7a23e87a4a628ee6
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-06-11 00:47:26 +0800

    Allow nil for within and outside metro manila attributes

--- app/models/courier_rate.rb
+++ app/models/courier_rate.rb
@@ -2,8 +2,8 @@ class CourierRate < ApplicationRecord
   scope :details, -> { select(:id, :name, :within_metro_manila, :outside_metro_manila, :delivery_option) }
   enum delivery_option: %i[on-demand next-day]
   validates :name, presence: true
-  validates :within_metro_manila, numericality: true
-  validates :outside_metro_manila, numericality: true
+  validates :within_metro_manila, numericality: true, allow_nil: true
+  validates :outside_metro_manila, numericality: true, allow_nil: true
 
   def self.attributes
     details.map do |courier|
