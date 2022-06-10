8d2f0e930139744d40edd6278f1d49d8c0d12ea0
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-06-10 23:33:30 +0800

    Add factory for address

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,10 +1,30 @@
 FactoryBot.define do
   factory :address do
-    address1 { "MyString" }
-    address2 { "MyString" }
-    city { "MyString" }
-    province { "MyString" }
-    country { "MyString" }
-    zip_code { "MyString" }
+    trait :pickup do
+      address1 { 'My Address' }
+      address2 { 'My Address 2' }
+      city { 'City' }
+      province { 'Metro Manila' }
+      country { 'Philippines' }
+      zip_code { '1000' }
+    end
+
+    trait :delivery_within do
+      address1 { 'New Address' }
+      address2 { 'New Address 2' }
+      city { 'New City' }
+      province { 'Metro Manila' }
+      country { 'Philippines' }
+      zip_code { '1001' }
+    end
+
+    trait :delivery_outside do
+      address1 { 'Test Address' }
+      address2 { 'Test Address 2' }
+      city { 'Test City' }
+      province { 'Bulacan' }
+      country { 'Philippines' }
+      zip_code { '1002' }
+    end
   end
 end
