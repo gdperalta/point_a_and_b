4bc65c63625b3dadb0f8bfa4473a5b3846f7e38a
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-06-10 20:51:13 +0800

    Add scopes and validations

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,6 @@
 class City < ApplicationRecord
-  belongs_to :province
+  scope :find_by_province, ->(province_id) { where('province_id = ?', province_id) }
   has_many :addresses
+  belongs_to :province
+  validates :name, presence: true
 end
--- app/models/province.rb
+++ app/models/province.rb
@@ -1,4 +1,6 @@
 class Province < ApplicationRecord
+  scope :find_by_country, ->(country_id) { where('country_id = ?', country_id) }
   belongs_to :country
   has_many :cities
+  validates :name, presence: true
 end
