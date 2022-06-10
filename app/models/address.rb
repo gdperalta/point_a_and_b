c7022cd8de703059093be2000d69a00e32ddf354
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-06-10 15:48:27 +0800

    Add methods for city, province, and country name

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,17 @@
 class Address < ApplicationRecord
   enum address_type: %i[pickup delivery]
   has_many :shipment_quotes
+  belongs_to :city
+
+  def city_name
+    city.name
+  end
+
+  def province_name
+    city.province.name
+  end
+
+  def country_name
+    province.country.name
+  end
 end
