618ae15f03f663e338fb4fc59c210c433853a5c2
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-06-10 20:50:56 +0800

    Add routes for locations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :shipment_quotes
+  get '/countries', to: 'countries#index', as: 'countries'
+  get '/provinces', to: 'provinces#index', as: 'provinces'
+  get '/countries/:country_id/provinces', to: 'provinces#find_by_country', as: 'country_provinces'
+  get '/cities', to: 'cities#index', as: 'cities'
+  get '/provinces/:province_id/cities', to: 'cities#find_by_province', as: 'province_cities'
 
   root 'shipment_quotes#index'
 end
