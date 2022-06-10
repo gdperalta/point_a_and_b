c13bb8ec7ef004a8b67a6e40ceb17e5387c3a523
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-06-11 00:48:28 +0800

    Edit factories

--- spec/factories/cities.rb
+++ spec/factories/cities.rb
@@ -1,6 +1,15 @@
 FactoryBot.define do
   factory :city do
-    province { nil }
-    name { "MyString" }
+    trait :city1 do
+      id { 1 }
+      province_id { 1 }
+      name { 'City 1' }
+    end
+
+    trait :city2 do
+      id { 2 }
+      province_id { 2 }
+      name { 'City 2' }
+    end
   end
 end
--- spec/factories/shipment_quotes.rb
+++ spec/factories/shipment_quotes.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :shipment_quote do
-    
+    trait :within do
+      id { 1 }
+      pickup_address_id { 1 }
+      delivery_address_id { 2 }
+    end
+
+    trait :outside do
+      id { 2 }
+      pickup_address_id { 1 }
+      delivery_address_id { 3 }
+    end
   end
 end
