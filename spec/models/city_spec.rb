1794b9aa194ad3a51e1dc6aab57e6636a565eaf5
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-06-13 12:06:51 +0800

    Add validations for locations

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe City, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:country) { create(:country, :ph) }
+  let!(:province) { create(:province, :metro_manila) }
+  let(:city) { build(:city, :city1) }
+
+  context 'validations' do
+    it 'is valid with valid attributes' do
+      expect(city).to be_valid
+    end
+  end
+
+  context 'address1' do
+    it 'is invalid if blank' do
+      city.name = ''
+      expect(city).to_not be_valid
+      expect(city.errors.to_hash.keys).to include(:name)
+      expect(city.errors[:name]).to include("can't be blank")
+    end
+  end
 end
